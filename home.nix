c1ac0dd1489379af8d6a46add9dbc75714b1d501
ishan <ishan365.ia@gmail.com>
2020-06-22 12:04:08 +0530

    first commit

---
+++ home.nix
@@ -0,0 +1,22 @@
+{ config, pkgs, ... }:
+
+{
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+  home.packages = [
+  pkgs.fzf
+  pkgs.ripgrep
+  pkgs.fd
+  pkgs.exa
+  ];
+  programs.bat.enable = true;
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "20.03";
+}
