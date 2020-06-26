c2921f015604e287f400f95dadf885ee3ec759b9
ishan <ishan365.ia@gmail.com>
2020-06-26 11:35:13 +0530

    mpd and ncmpcpp

--- home.nix
+++ home.nix
@@ -20,6 +20,8 @@
   programs.direnv.enable = true;
   programs.direnv.enableFishIntegration = true;
 
+  services.mpd.enable = true;
+  services.mpd.musicDirectory = "/home/me/Music";
   # This value determines the Home Manager release that your
   # configuration is compatible with. This helps avoid breakage
   # when a new Home Manager release introduces backwards
