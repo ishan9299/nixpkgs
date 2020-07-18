c62929912475a2cf227ec975d3af7166b558c640
ishan <ishan365.ia@gmail.com>
2020-07-18 13:50:38 +0530

    fixed the indentation and tmux is launched by default in alacritty

--- home.nix
+++ home.nix
@@ -3,45 +3,47 @@
 {
   home.username = "me";
   home.homeDirectory = "/home/me";
-  
+
   home.sessionVariables = {
-      EDITOR="nvim";
-      WEECHAT_HOME="~/.config/weechat";
-};
+    EDITOR="nvim";
+    WEECHAT_HOME="~/.config/weechat";
+  };
 
+  # home.keyboard.options = [ "grp:caps_toggle" ];
 
-gtk.gtk3.css = ''
-window.ssd separator:first-child + headerbar:backdrop,
-window.ssd separator:first-child + headerbar,
-window.ssd headerbar:first-child:backdrop,
-window.ssd headerbar:first-child,
-window.ssd headerbar:last-child:backdrop,
-window.ssd headerbar:last-child,
-window.ssd stack headerbar:first-child:backdrop,
-window.ssd stack headerbar:first-child,
-window.ssd stack headerbar:last-child:backdrop,
-window.ssd stack headerbar:last-child,
-window.ssd decoration,
-window.ssd headerbar.titlebar {
-  border-radius: 0;
-}
+  gtk.enable = true;
+  gtk.gtk3.extraCss = ''
+    window.ssd separator:first-child + headerbar:backdrop,
+    window.ssd separator:first-child + headerbar,
+    window.ssd headerbar:first-child:backdrop,
+    window.ssd headerbar:first-child,
+    window.ssd headerbar:last-child:backdrop,
+    window.ssd headerbar:last-child,
+    window.ssd stack headerbar:first-child:backdrop,
+    window.ssd stack headerbar:first-child,
+    window.ssd stack headerbar:last-child:backdrop,
+    window.ssd stack headerbar:last-child,
+    window.ssd decoration,
+    window.ssd headerbar.titlebar {
+    border-radius: 0;
+    }
 
-window.ssd headerbar * {
-  margin-top: -100px;
-  color: #1c2022; /* May need to tweak, this matches Adwaita Dark */
-}
+    window.ssd headerbar * {
+    margin-top: -100px;
+    color: #1c2022; /* May need to tweak, this matches Adwaita Dark */
+    }
 
-window.ssd headerbar.titlebar,
-window.ssd headerbar.titlebar button.titlebutton {
-  border: none;
-  font-size: 0;
-  height: 0;
-  margin: 0;
-  max-height: 0;
-  min-height: 0;
-  padding: 0;
-}
-'';
+    window.ssd headerbar.titlebar,
+    window.ssd headerbar.titlebar button.titlebutton {
+    border: none;
+    font-size: 0;
+    height: 0;
+    margin: 0;
+    max-height: 0;
+    min-height: 0;
+    padding: 0;
+    }
+  '';
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
@@ -55,12 +57,12 @@ window.ssd headerbar.titlebar button.titlebutton {
     pkgs.lf
     pkgs.stow
     pkgs.ncmpcpp
-    pkgs.mpd
     pkgs.youtube-dl
     pkgs.mpc_cli
     pkgs.weechat
     pkgs.cava
     pkgs.tree
+    pkgs.blender
   ];
   home.sessionVariables = {
     LANG = "en_US.UTF-8";
@@ -71,7 +73,7 @@ window.ssd headerbar.titlebar button.titlebutton {
   programs.bat = {
     enable = true;
     config = {
-    theme = "Solarized (light)";
+      theme = "Solarized (light)";
     };
   };
 
@@ -104,15 +106,50 @@ window.ssd headerbar.titlebar button.titlebutton {
     treeView = true;
   };
 
-  programs.tmux = {
+  programs.alacritty = {
     enable = true;
-    baseIndex = 1;
-    keyMode = "vi";
-    escapeTime = 0;
-    customPaneNavigationAndResize = true;
-    sensibleOnTop = false;
-    terminal = "screen-256color";
-    extraConfig = ''
+    settings = {
+
+      window = {
+        padding = {
+          x = 0;
+          y = 0;
+        };
+      };
+
+      font = {
+        normal = {
+          family = "Source Code Pro";
+          style = "Regular";
+        };
+        bold = {
+          family = "Source Code Pro";
+          style = "Regular";          
+        };
+        italic = {
+          family = "Source Code Pro";
+          style = "Regular";
+        };
+        size = 14;
+      };
+      shell.program = /home/me/.nix-profile/bin/tmux;
+    };
+  };
+
+    programs.kitty = {
+      enable = true;
+      font.name = "Source Code Pro 12";
+    };
+
+    programs.tmux = {
+      enable = true;
+      baseIndex = 1;
+      keyMode = "vi";
+      escapeTime = 0;
+      customPaneNavigationAndResize = true;
+      sensibleOnTop = false;
+      terminal = "screen-256color";
+      extraConfig = ''
 #+--------- Random Config -------+
       set-option -g mouse on
       set-option -sa terminal-overrides ',xterm-256color*:Tc'
@@ -143,41 +180,41 @@ window.ssd headerbar.titlebar button.titlebutton {
 #+---------- Alignment ----------+
       set-option -g status-justify centre
     '';
-  };
+    };
 
-  programs.rtorrent = {
-    enable = true;
-  };
+    programs.rtorrent = {
+      enable = true;
+    };
 
-  programs.fish = {
-    enable = true;
-    shellInit = ''
+    programs.fish = {
+      enable = true;
+      shellInit = ''
       set PATH $HOME/.local/bin $HOME/.local/npm/bin $PATH
       set PATH /var/lib/flatpak/exports/bin $PATH
 
 # For nixpkgs make sure glibcLocales is installed with the nixpkgs or home manager
-      bass source /home/me/.nix-profile/etc/profile.d/nix.sh
-      export LOCALE_ARCHIVE=$HOME/.nix-profile/lib/locale/locale-archive
+      # bass source /home/me/.nix-profile/etc/profile.d/nix.sh
+      # export LOCALE_ARCHIVE=$HOME/.nix-profile/lib/locale/locale-archive
     '';
 
-    plugins = [
-      {
-        name = "bass";
-        src = pkgs.fetchFromGitHub {
-          owner = "edc";
-          repo = "bass";
-          rev = "50eba266b0d8a952c7230fca1114cbc9fbbdfbd4";
-          sha256 = "0ppmajynpb9l58xbrcnbp41b66g7p0c9l2nlsvyjwk6d16g4p4gy";
-        };
-      }
-    ];
-  };
+      plugins = [
+        {
+          name = "bass";
+          src = pkgs.fetchFromGitHub {
+            owner = "edc";
+            repo = "bass";
+            rev = "50eba266b0d8a952c7230fca1114cbc9fbbdfbd4";
+            sha256 = "0ppmajynpb9l58xbrcnbp41b66g7p0c9l2nlsvyjwk6d16g4p4gy";
+          };
+        }
+      ];
+    };
 
-  services.mpd = {
-    enable = true;
-    musicDirectory = "/home/me/Music";
-    dataDir = "/home/me/.local/share/mpd";
-    extraConfig = ''
+    services.mpd = {
+      enable = true;
+      musicDirectory = "/home/me/Music";
+      dataDir = "/home/me/.local/share/mpd";
+      extraConfig = ''
       auto_update "yes"
       log_file "syslog"
 
@@ -186,15 +223,15 @@ window.ssd headerbar.titlebar button.titlebutton {
       name "pulse audio"
       }
     '';
-  };
+    };
 
-  # This value determines the Home Manager release that your
-  # configuration is compatible with. This helps avoid breakage
-  # when a new Home Manager release introduces backwards
-  # incompatible changes.
-  #
-  # You can update Home Manager without changing this value. See
-  # the Home Manager release notes for a list of state version
-  # changes in each release.
-  home.stateVersion = "20.09";
-}
+    # This value determines the Home Manager release that your
+    # configuration is compatible with. This helps avoid breakage
+    # when a new Home Manager release introduces backwards
+    # incompatible changes.
+    #
+    # You can update Home Manager without changing this value. See
+    # the Home Manager release notes for a list of state version
+    # changes in each release.
+    home.stateVersion = "20.09";
+  }
